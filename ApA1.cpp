e7f0344f251aac1106079386eca187737187fd4e
tiom4eg <62618821+tiom4eg@users.noreply.github.com>
2023-08-21 13:05:27 +0300

    Create ApA1.cpp

---
+++ ApA1.cpp
@@ -0,0 +1,235 @@
+#include <bits/stdc++.h>
+// tiom4eg's precompiler options
+// POGGERS POGGERS POGGERS POGGERS POGGERS POGGERS POGGERS
+// IO settings
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(0)
+// Quick types
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define pii pair <int, int>
+#define vi vector <int>
+#define mi vector <vector <int>>
+// Quick functions
+#define endl '\n'
+#define F first
+#define S second
+#define all(a) a.begin(), a.end()
+#define sz(a) (int)(a.size())
+#define pb push_back
+#define mp make_pair
+// Quick fors
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define FORS(i, a, b, c) for (int i = a; i < b; i += c)
+#define RFOR(i, a, b) for (int i = a; i >= b; --i)
+#define EACH(e, a) for (auto& e : a)
+// Pragmas
+#ifndef TIOM4EG
+#pragma GCC optimize("O3") // let the chaos begin!
+#pragma GCC target("avx,avx2,tune=native")
+#pragma GCC comment(linker, "/stack:200000000")
+#endif
+// PBDS
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pbds tree <int, null_type, less <int>, rb_tree_tag, tree_order_statistics_node_update>
+using namespace __gnu_pbds;
+// POGGERS POGGERS POGGERS POGGERS POGGERS POGGERS POGGERS
+using namespace std;
+mt19937 rng(chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count());
+//#define int long long
+constexpr int INF = 1e9 + 7, MD = 998244353, MAX = 500007, LG = 18, R = 1 << LG, MOD = 1040015701, MOD2 = 1e9 + 9, RB = 158;
+const ll INFLL = 1e18 + 7;
+
+/// source: https://github.com/tiom4eg/code-library/blob/master/Geometry/Geometry%202D.cpp
+const double inf = 1e100;
+const double eps = 1e-9;
+const double PI = acos((double)-1.0);
+int sign(double x) { return (x > eps) - (x < -eps); }
+struct PT {
+    double x, y;
+    PT() { x = 0, y = 0; }
+    PT(double x, double y) : x(x), y(y) {}
+    PT(const PT &p) : x(p.x), y(p.y)    {}
+    PT operator + (const PT &a) const { return PT(x + a.x, y + a.y); }
+    PT operator - (const PT &a) const { return PT(x - a.x, y - a.y); }
+    PT operator * (const double a) const { return PT(x * a, y * a); }
+    friend PT operator * (const double &a, const PT &b) { return PT(a * b.x, a * b.y); }
+    PT operator / (const double a) const { return PT(x / a, y / a); }
+    bool operator == (PT a) const { return sign(a.x - x) == 0 && sign(a.y - y) == 0; }
+    bool operator != (PT a) const { return !(*this == a); }
+    bool operator < (PT a) const { return sign(a.x - x) == 0 ? y < a.y : x < a.x; }
+    bool operator > (PT a) const { return sign(a.x - x) == 0 ? y > a.y : x > a.x; }
+    double norm() { return sqrt(x * x + y * y); }
+    double norm2() { return x * x + y * y; }
+    PT perp() { return PT(-y, x); }
+    double arg() { return atan2(y, x); }
+    PT truncate(double r) { // returns a vector with norm r and having same direction
+        double k = norm();
+        if (!sign(k)) return *this;
+        r /= k;
+        return PT(x * r, y * r);
+    }
+};
+inline double dot(PT a, PT b) { return a.x * b.x + a.y * b.y; }
+inline double dist2(PT a, PT b) { return dot(a - b, a - b); }
+inline double dist(PT a, PT b) { return sqrt(dot(a - b, a - b)); }
+inline double cross(PT a, PT b) { return a.x * b.y - a.y * b.x; }
+inline double cross2(PT a, PT b, PT c) { return cross(b - a, c - a); }
+inline int orientation(PT a, PT b, PT c) { return sign(cross(b - a, c - a)); }
+PT perp(PT a) { return PT(-a.y, a.x); }
+PT rotateccw90(PT a) { return PT(-a.y, a.x); }
+PT rotatecw90(PT a) { return PT(a.y, -a.x); }
+PT rotateccw(PT a, double t) { return PT(a.x * cos(t) - a.y * sin(t), a.x * sin(t) + a.y * cos(t)); }
+PT rotatecw(PT a, double t) { return PT(a.x * cos(t) + a.y * sin(t), -a.x * sin(t) + a.y * cos(t)); }
+double SQ(double x) { return x * x; }
+
+bool half(PT p) {
+    return p.y > 0.0 || (p.y == 0.0 && p.x < 0.0);
+}
+void polar_sort(vector<PT> &v) { // sort points in counterclockwise
+    sort(v.begin(), v.end(), [](PT a,PT b) {
+        return make_tuple(half(a), 0.0, a.norm2()) < make_tuple(half(b), cross(a, b), b.norm2());
+    });
+}
+void polar_sort(vector<PT> &v, PT o) { // sort points in counterclockwise with respect to point o
+    sort(v.begin(), v.end(), [&](PT a,PT b) {
+        return make_tuple(half(a - o), 0.0, (a - o).norm2()) < make_tuple(half(b - o), cross(a - o, b - o), (b - o).norm2());
+    });
+}
+
+struct line {
+    PT a, b; // goes through points a and b
+    PT v; double c;  //line form: direction vec [cross] (x, y) = c
+    line() {}
+    //direction vector v and offset c
+	line(PT v, double c) : v(v), c(c) {
+        auto p = get_points();
+        a = p.first; b = p.second;
+	}
+	// equation ax + by + c = 0
+	line(double _a, double _b, double _c) : v({_b, -_a}), c(-_c) {
+		auto p = get_points();
+        a = p.first; b = p.second;
+	}
+	// goes through points p and q
+	line(PT p, PT q) : v(q - p), c(cross(v, p)), a(p), b(q) {}
+    	pair<PT, PT> get_points() { //extract any two points from this line
+		PT p, q; double a = -v.y, b = v.x; // ax + by = c
+		if (sign(a) == 0) {
+		    p = PT(0, c / b);
+		    q = PT(1, c / b);
+		}
+		else if (sign(b) == 0) {
+		    p = PT(c / a, 0);
+		    q = PT(c / a, 1);
+		}
+		else {
+		    p = PT(0, c / b);
+		    q = PT(1, (c - a) / b);
+		}
+		return {p, q};
+    	}
+    //ax + by + c = 0
+    array<double, 3> get_abc() {
+        double a = -v.y, b = v.x;
+        return {a, b, c};
+    }
+    // 1 if on the left, -1 if on the right, 0 if on the line
+    int side(PT p) { return sign(cross(v, p) - c); }
+    // line that is perpendicular to this and goes through point p
+    line perpendicular_through(PT p) { return {p, p + perp(v)}; }
+    // translate the line by vector t i.e. shifting it by vector t
+    line translate(PT t) { return {v, c + cross(v, t)}; }
+    // compare two points by their orthogonal projection on this line
+    // a projection point comes before another if it comes first according to vector v
+    bool cmp_by_projection(PT p, PT q) { return dot(v, p) < dot(v, q); }
+	line shift_left(double d) {
+		PT z = v.perp().truncate(d);
+		return line(a + z, b + z);
+	}
+};
+// find a point from a through b with distance d
+PT point_along_line(PT a, PT b, double d) {
+    assert(a != b);
+    return a + (((b - a) / (b - a).norm()) * d);
+}
+// projection point c onto line through a and b  assuming a != b
+PT project_from_point_to_line(PT a, PT b, PT c) {
+    return a + (b - a) * dot(c - a, b - a) / (b - a).norm2();
+}
+// reflection point c onto line through a and b  assuming a != b
+PT reflection_from_point_to_line(PT a, PT b, PT c) {
+    PT p = project_from_point_to_line(a,b,c);
+    return p + p - c;
+}
+// minimum distance from point c to line through a and b
+double dist_from_point_to_line(PT a, PT b, PT c) {
+    return fabs(cross(b - a, c - a) / (b - a).norm());
+}
+// 0 if not parallel, 1 if parallel, 2 if collinear
+bool are_lines_same(PT a, PT b, PT c, PT d) {
+    if (fabs(cross(a - c, c - d)) < eps && fabs(cross(b - c, c - d)) < eps) return true;
+    return false;
+}
+// 1 if point is ccw to the line, 2 if point is cw to the line, 3 if point is on the line
+int point_line_relation(PT a, PT b, PT p) {
+    int c = sign(cross(p - a, b - a));
+    if (c < 0) return 1;
+    if (c > 0) return 2;
+    return 3;
+}
+// returns true if  point p is on line segment ab
+bool is_point_on_seg(PT a, PT b, PT p) {
+    if (fabs(cross(p - b, a - b)) < eps) {
+        if (p.x < min(a.x, b.x) || p.x > max(a.x, b.x)) return false;
+        if (p.y < min(a.y, b.y) || p.y > max(a.y, b.y)) return false;
+        return true;
+    }
+    return false;
+}
+vector <PT> convex_hull(vector<PT> &p) {
+	if (p.size() <= 1) return p;
+	vector<PT> v = p;
+    sort(v.begin(), v.end());
+    vector<PT> up, dn;
+    for (auto& p : v) {
+        while (up.size() > 1 && orientation(up[up.size() - 2], up.back(), p) >= 0) {
+            up.pop_back();
+        }
+        while (dn.size() > 1 && orientation(dn[dn.size() - 2], dn.back(), p) <= 0) {
+            dn.pop_back();
+        }
+        up.push_back(p);
+        dn.push_back(p);
+    }
+    v = dn;
+    if (v.size() > 1) v.pop_back();
+    reverse(up.begin(), up.end());
+    up.pop_back();
+    for (auto& p : up) {
+        v.push_back(p);
+    }
+    if (v.size() == 2 && v[0] == v[1]) v.pop_back();
+    return v;
+}
+///
+
+int n;
+
+inline int nx(int i) { return i == n - 1 ? 0 : i + 1; }
+
+signed main() {
+    fastIO;
+    double xc, yc; cin >> n >> xc >> yc;
+    PT o(xc, yc);
+    vector <PT> p(n);
+    FOR(i, 0, n) cin >> p[i].x >> p[i].y;
+    vector <PT> h = convex_hull(p);
+    int cnt = 0;
+    FOR(i, 0, sz(h)) {
+        PT z = project_from_point_to_line(h[i], h[(i == sz(h) - 1) ? 0 : i + 1], o);
+        cnt += (z != h[i] && z != h[(i == sz(h) - 1) ? 0 : i + 1] && is_point_on_seg(h[i], h[(i == sz(h) - 1) ? 0 : i + 1], z));
+    }
+    cout << cnt;
+}
