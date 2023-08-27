c84ec60a6645c8e49afaf65b4d098f7376173810
tiom4eg <62618821+tiom4eg@users.noreply.github.com>
2023-08-27 14:31:41 +0300

    Update A6-debug.cpp

--- A6-debug.cpp
+++ A6-debug.cpp
@@ -59,9 +59,9 @@ struct fig {
     bool operator<(fig o) { return (diag(0) == o.diag(0)) ? (diag(1) < o.diag(1)) : (diag(0) < o.diag(0)); } // OK
     void shift(int d, int k) { // moves by k on d-diag, OK
         if (won) return;
-        cout << "algo: " << x << ' ' << y << ", diags: " << diag(0) << ' ' << diag(1) << " | ";
+        //cout << "algo: " << x << ' ' << y << ", diags: " << diag(0) << ' ' << diag(1) << " | ";
         x += k, y += (d ? 1 : -1) * k;
-        cout << x << ' ' << y << ", diags: " << diag(0) << ' ' << diag(1) << endl;
+        //cout << x << ' ' << y << ", diags: " << diag(0) << ' ' << diag(1) << endl;
         auto_interact();
         //cin >> ax >> ay >> kx >> ky;
         if (!ax && !ay && !kx && !ky) won = 1;
@@ -84,96 +84,113 @@ void auto_interact() {
         kx -= dx, ky -= dy;
         if (f) {
             ax = kx, ay = ky;
-            cout << "interactor: " << kx << ' ' << ky << ' ';
+            //cout << "interactor: " << kx << ' ' << ky << ' ';
             kx += dx, ky += dy;
-            cout << kx << ' ' << ky << endl;
+            //cout << kx << ' ' << ky << endl;
             return;
         }
     }
     ax = ay = kx = ky = 0;
-    cout << "interactor: 0 0 0 0" << endl;
+    //cout << "interactor: 0 0 0 0" << endl;
 }
 
 
-void solve() {
-    won = 0;
-    a[0].clear(), a[1].clear();
-    FOR(i, 0, 5) {
-        int x, y; cin >> x >> y;
-        fig f(x, y);
-        a[f.color()].pb(f);
-    }
-    cin >> kx >> ky;
-    if (sz(a[0]) < 2 || sz(a[1]) < 2) return void(cout << "algo: 0 0 0 0" << endl);
+int solve() {
+    if (sz(a[0]) < 2 || sz(a[1]) < 2) return 0;
     /// phase 1 (2 moves max)
     int f = (sz(a[0]) > sz(a[1]));
     if (f) swap(a[0], a[1]); // we need to ensure that 2 figures of same color are secured first
     sort(all(a[0]));
     if (!a[0][0].same(a[0][1], 0) && !a[0][0].same(a[0][1], 1)) a[0][1].move_to(0, a[0][0].diag(0));
-    if (won) return;
+    if (won) return 0;
     FOR(i, 0, 3) if (kx == a[1][i].x && ky == a[1][i].y) a[1].erase(a[1].begin() + i); // if king eaten smth
     sort(all(a[1]));
     if (!a[1][0].same(a[1][1], 0) && !a[1][0].same(a[1][1], 1)) a[1][1].move_to(0, a[1][0].diag(0));
-    if (won) return;
-    cout << "PHASE 1 END" << endl;
+    if (won) return 0;
     if (f) swap(a[0], a[1]);
     sort(all(a[0])), sort(all(a[1]));
     /// phase 2 (7 moves max)
     a[0][0].move_to((a[0][0].diag(0) == a[0][1].diag(0)), -100000000);
-    if (won) return;
+    if (won) return 0;
     a[0][1].move_to((a[0][0].diag(0) == a[0][1].diag(0)), -10000000);
-    if (won) return;
+    if (won) return 0;
     a[0][1].move_to((a[0][0].diag(0) != a[0][1].diag(0)), a[0][1].diag(a[0][0].diag(0) != a[0][1].diag(0)) - 1000000);
-    if (won) return;
+    if (won) return 0;
     a[1][0].move_to((a[1][0].diag(0) == a[1][1].diag(0)), -100000001);
-    if (won) return;
+    if (won) return 0;
     a[1][1].move_to((a[1][0].diag(0) == a[1][1].diag(0)), -10000001);
-    if (won) return;
+    if (won) return 0;
     a[1][1].move_to((a[1][0].diag(0) != a[1][1].diag(0)), a[1][1].diag(a[1][0].diag(0) != a[1][1].diag(0)) - 1000000);
-    if (won) return;
+    if (won) return 0;
     // third is useless and may fuck us up
     if (sz(a[0]) > 2) {
         a[0][2].move_to(0, 200000000);
-        if (won) return;
+        if (won) return 0;
     }
     if (sz(a[1]) > 2) {
         a[1][2].move_to(0, 200000001);
-        if (won) return;
+        if (won) return 0;
     }
-    cout << "PHASE 2 END" << endl;
     /// phase 3 (block by 0-diag, 28 moves max)
     int d0 = kx + ky;
     int l = d0 - 8;
     a[!(d0 & 1)][0].jump_to(d0 - 7);
-    if (won) return;
+    if (won) return 0;
     a[d0 & 1][0].jump_to(d0 - 8);
-    if (won) return;
+    if (won) return 0;
     d0 = kx + ky;
     int r = d0 + 8;
     a[!(d0 & 1)][1].jump_to(d0 + 7);
-    if (won) return;
+    if (won) return 0;
     a[d0 & 1][1].jump_to(d0 + 8);
-    if (won) return;
+    if (won) return 0;
     while (l + 4 < r) {
         if (kx + ky > l + 2) a[l & 1][0].shift(1, 1), ++l;
         else a[r & 1][1].shift(1, -1), --r;
-        if (won) return;
+        if (won) return 0;
     }
-    cout << "PHASE 3 END" << endl;
     /// phase 4 (block by 1-diag, 3 moves max)
     int col = (kx + ky) & 1;
     a[col][0].move_to(1, (kx - ky) - 2); // block bottom
-    if (won) return;
+    if (won) return 0;
     a[col][1].move_to(1, (kx - ky) + 2); // block top
-    if (won) return;
+    if (won) return 0;
     a[col][1].move_to(1, (kx - ky) + 2); // stalemate
-    if (!won) {
-        cout << "IM DEAD LMAO" << endl;
-    }
+    if (won) return 0;
+    return 1;
 }
 
 signed main() {
     //fastIO;
-    int tc; cin >> tc;
-    while (tc--) solve();
+    while (true) {
+        won = 0;
+        a[0].clear(), a[1].clear();
+        vi xs(5), ys(5);
+        FOR(i, 0, 5) {
+            int x = (rng() % 21) - 10, y = (rng() % 21) - 10;
+            while (true) {
+                int f = 1;
+                FOR(j, 0, i) if (x == xs[j] && y == ys[j]) f = 0;
+                if (f) break;
+                x = (rng() % 21) - 10, y = (rng() % 21) - 10;
+            }
+            xs[i] = x, ys[i] = y;
+            fig f(x, y);
+            a[f.color()].pb(f);
+        }
+        int skx = (rng() % 21) - 10, sky = (rng() % 21) - 10;
+        while (true) {
+            int f = 1;
+            EACH(e, a[0]) if (skx + sky == e.diag(0) || skx - sky == e.diag(1)) f = 0;
+            EACH(e, a[0]) if (skx + sky == e.diag(0) || skx - sky == e.diag(1)) f = 0;
+            if (f) break;
+            skx = (rng() % 21) - 10, sky = (rng() % 21) - 10;
+        }
+        kx = skx, ky = sky;
+        if (solve()) {
+            FOR(i, 0, 5) cout << xs[i] << ' ' << ys[i] << ' ';
+            cout << skx << ' ' << sky;
+            return 0;
+        }
+    }
 }
